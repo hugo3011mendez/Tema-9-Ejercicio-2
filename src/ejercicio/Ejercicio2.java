92bde0d80c251d8d44187862f955f72033ebc3ae
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-25 17:33:29 +0200

    Creado código para mostrar el formulario

--- src/ejercicio/Ejercicio2.java
+++ src/ejercicio/Ejercicio2.java
@@ -3,5 +3,12 @@ package ejercicio;
 import javax.swing.*;
 
 public class Ejercicio2 {
-    
+    public static void main(String[] args) {
+        FrameEjercicio2 frame = new FrameEjercicio2();
+
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
 }
