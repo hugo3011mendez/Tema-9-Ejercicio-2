b9a3cd10cdc17fa322d2dadd00d8bac526f7669f
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-25 20:38:28 +0200

    Añadida barra de menú con funcionalidades Reset y Acerca De

--- src/ejercicio/FrameEjercicio2.java
+++ src/ejercicio/FrameEjercicio2.java
@@ -14,6 +14,11 @@ public class FrameEjercicio2 extends JFrame implements ActionListener{
     JTextField txfNumerosPulsados;
     JButton btnReset;
 
+    // Declaración de elementos del menú
+    JMenuBar menu;
+    JMenu mnuArchivo, mnuMovil, mnuOtros;
+    JMenuItem mnuGrabar, mnuLeer, mnuReset, mnuSalir, mnuAcercaDe; 
+
     int x = 100, y = 10; // Creo variables para controlar las coordenadas de los botones
     String[] botones = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "0", "*"}; // Creo un array con el texto que debe contener cada botón
     ArrayList<JButton> teclasTelefono = new ArrayList<>(); // Creo una colección para guardar los JButton creados para el teléfono
@@ -27,16 +32,14 @@ public class FrameEjercicio2 extends JFrame implements ActionListener{
         class MouseHandler extends MouseAdapter { // Creo la clase MouseHandler con el adaptador
 
             @Override
-            public void mouseEntered(MouseEvent e) { // Cambio el color de fondo cuando está sobre un botón que no ha
-                                                     // sido pulsado
+            public void mouseEntered(MouseEvent e) { // Cambio el color de fondo cuando está sobre un botón que no ha sido pulsado
                 if (((JButton) e.getSource()).getBackground() != Color.GRAY) {
                     ((JButton) e.getSource()).setBackground(Color.RED);
                 }
             }
 
             @Override
-            public void mouseExited(MouseEvent e) { // Cambio el color de fondo cuando sale de un botón que no ha sido
-                                                    // pulsado
+            public void mouseExited(MouseEvent e) { // Cambio el color de fondo cuando sale de un botón que no ha sido pulsado
                 if (((JButton) e.getSource()).getBackground() != Color.GRAY) {
                     ((JButton) e.getSource()).setBackground(null);
                 }
@@ -81,6 +84,52 @@ public class FrameEjercicio2 extends JFrame implements ActionListener{
         addKeyListener(kh); // Añado el KeyListener para controlar los eventos de teclado
 
 
+        // Muestro la barra de menú y sus elementos en el formulario
+        // Menú Archivo
+        mnuGrabar = new JMenuItem("Grabar");
+        mnuGrabar.setMnemonic('G');
+        mnuGrabar.addActionListener(this);
+
+        mnuLeer = new JMenuItem("Leer");
+        mnuLeer.setMnemonic('L');
+        mnuLeer.addActionListener(this);
+
+        mnuArchivo = new JMenu("Archivo");
+        mnuArchivo.setMnemonic('A');
+        mnuArchivo.add(mnuGrabar);
+        mnuArchivo.add(mnuLeer);
+
+        // Menú Móvil
+        mnuReset = new JMenuItem("Reset");
+        mnuReset.setMnemonic('R');
+        mnuReset.addActionListener(this);
+
+        mnuSalir = new JMenuItem("Salir");
+        mnuSalir.setMnemonic('S');
+        mnuSalir.addActionListener(this);
+
+        mnuMovil = new JMenu("Movil");
+        mnuMovil.setMnemonic('M');
+        mnuMovil.add(mnuReset);
+        mnuMovil.addSeparator();
+        mnuMovil.add(mnuSalir);
+
+        // Menú Otros
+        mnuAcercaDe = new JMenuItem("Acerca De");
+        mnuAcercaDe.addActionListener(this);
+
+        mnuOtros = new JMenu("Otros");
+        mnuOtros.setMnemonic('O');
+        mnuOtros.add(mnuAcercaDe);
+
+        // Barra de Menú
+        menu = new JMenuBar();
+        menu.add(mnuArchivo);
+        menu.add(mnuMovil);
+        menu.add(mnuOtros);
+        this.setJMenuBar(menu);
+
+
         for (int i = 0; i < botones.length; i++) {
             if (i % 3 == 0) { // Para que cada fila sea de 3 botones, cada 3 botones aumento la coordenada Y y devuelvo la coordenada X a su valor inicial
                 y += 30;
@@ -116,12 +165,16 @@ public class FrameEjercicio2 extends JFrame implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == btnReset){ // Acciones a realizar cuando se pulsa el botón reset
+        if(e.getSource() == btnReset || e.getSource() == mnuReset){ // Acciones a realizar cuando se pulsa el botón reset o el MenuItem Reset
             txfNumerosPulsados.setText("");
 
             for (int i = 0; i < teclasTelefono.size(); i++) {
                 teclasTelefono.get(i).setBackground(null);
             }
+        }
+        else if(e.getSource() == mnuAcercaDe){ // Acciones a realizar al pulsar la opción de menú "Acerca De"
+            JOptionPane.showMessageDialog(null, "Este programa ha sido creado por el alumno Hugo Méndez González",
+            "Acerca del programa", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 }
