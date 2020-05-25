248b0a453fb092935519d82ee0d7635653964847
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-25 19:26:04 +0200

    Añadidas funciones del teclado y funciones del botón reiniciar a medias

--- src/ejercicio/FrameEjercicio2.java
+++ src/ejercicio/FrameEjercicio2.java
@@ -10,41 +10,63 @@ import javax.swing.*;
 import javax.swing.event.MouseInputListener;
 import javax.swing.filechooser.*;
 
-public class FrameEjercicio2 extends JFrame implements ActionListener {
+public class FrameEjercicio2 extends JFrame implements ActionListener{
     JTextField txfNumerosPulsados;
-    
+    JButton btnReset;
+
+    int x = 100, y = 10; // Creo variables para controlar las coordenadas de los botones
+    String[] botones = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "0", "*"}; // Creo un array con el texto que debe contener cada botón
+    ArrayList<JButton> teclasTelefono = new ArrayList<>(); // Creo una colección para guardar los JButton creados para el teléfono
 
     public FrameEjercicio2() {
         super("Tema 9 Ejercicio 2");
         setLayout(null);
+        setFocusable(true);
+
 
         class MouseHandler extends MouseAdapter { // Creo la clase MouseHandler con el adaptador
 
             @Override
-            public void mouseEntered(MouseEvent e) {
-                if(((JButton) e.getSource()).getBackground() != Color.GRAY){
+            public void mouseEntered(MouseEvent e) { // Cambio el color de fondo cuando está sobre un botón que no ha
+                                                     // sido pulsado
+                if (((JButton) e.getSource()).getBackground() != Color.GRAY) {
                     ((JButton) e.getSource()).setBackground(Color.RED);
                 }
             }
 
             @Override
-            public void mouseExited(MouseEvent e) {
-                if(((JButton) e.getSource()).getBackground() != Color.GRAY){
+            public void mouseExited(MouseEvent e) { // Cambio el color de fondo cuando sale de un botón que no ha sido
+                                                    // pulsado
+                if (((JButton) e.getSource()).getBackground() != Color.GRAY) {
                     ((JButton) e.getSource()).setBackground(null);
                 }
             }
 
             @Override
-            public void mouseClicked(MouseEvent e) {
+            public void mouseClicked(MouseEvent e) { // Cambio el color de fondo cuando pulsa un botón
                 ((JButton) e.getSource()).setBackground(Color.GRAY);
             }
 
         }
 
-        MouseHandler mh = new MouseHandler(); // Creo el MouseHandler
+        class KeyHandler extends KeyAdapter { // Creo la clase KeyHandler con el adaptador
 
-        int x = 100, y = 10; // Creo variables para controlar las coordenadas de los botones
-        String[] botones = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "#", "0", "*"}; // Creo un array con el texto que debe contener cada botón
+            @Override
+            public void keyTyped(KeyEvent e) {
+                System.err.println(e.getKeyChar());
+                for (int i = 0; i < teclasTelefono.size(); i++) {
+                    if(Character.toString(e.getKeyChar()).equals(teclasTelefono.get(i).getText())){ // Comparo la tecla pulsada con los botones del teléfono
+                        teclasTelefono.get(i).setBackground(Color.GRAY); // Si coincide, cambio el color de fondo del botón
+                    }
+                }
+            } 
+
+        }
+
+        MouseHandler mh = new MouseHandler(); // Creo el MouseHandler
+        KeyHandler kh = new KeyHandler(); // Creo el KeyHandler
+        addKeyListener(kh); // Añado el KeyListener para controlar los eventos de teclado
+
 
         for (int i = 0; i < botones.length; i++) {
             if (i % 3 == 0) { // Para que cada fila sea de 3 botones, cada 3 botones aumento la coordenada Y y devuelvo la coordenada X a su valor inicial
@@ -58,11 +80,34 @@ public class FrameEjercicio2 extends JFrame implements ActionListener {
             btnTelefono.addMouseListener(mh); // Añado el MouseHandler a todos los botones
             add(btnTelefono);
             x += 45; // Cada vez que añado un nuevo botón, aumento la coordenada X
+
+            teclasTelefono.add(btnTelefono);
         }
+
+        // Creo el TextField
+        txfNumerosPulsados = new JTextField(30);
+        txfNumerosPulsados.setLocation(50, 200);
+        txfNumerosPulsados.setSize(txfNumerosPulsados.getPreferredSize());
+        txfNumerosPulsados.setEditable(false);
+        txfNumerosPulsados.addActionListener(this);
+        add(txfNumerosPulsados);
+
+        // Creo el botón de resetear
+        btnReset = new JButton("Reset");
+        btnReset.setLocation(100, 250);
+        btnReset.setSize(btnReset.getPreferredSize());
+        btnReset.addActionListener(this);
+        add(btnReset);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        if(e.getSource() == btnReset){
+            //TODO Limpiar el TextField
+
+            for (int i = 0; i < teclasTelefono.size(); i++) {
+                teclasTelefono.get(i).setBackground(null);
+            }
+        }
     }
 }
